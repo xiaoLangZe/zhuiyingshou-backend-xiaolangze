3f4fba1461b56f6469fff02d58576ab05adeb1d6
xiaoLangZe <xshaoze@qq.com>
2026-02-07 03:18:55 +0800

    更新一些不可告人的秘密

--- src/lib/sendEmail/sendEmail.ts
+++ src/lib/sendEmail/sendEmail.ts
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
 
-// 这里本应该保密的（）
+// 这里本应该保密的（完成变成秘密变量）
 const resend = new Resend('re_gGhri8wc_GimAAGy5VDTmjKe48bLvNk7XJx');
 
 export async function sendEmail(to: [string], title: string, html: string){
